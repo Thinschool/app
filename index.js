e1a534e9b9a4f3601afce40ae8ea9a6b3299b857
Dhruv Jain <djain@clemson.edu>
2018-06-17 20:37:42 -0400

    added exclaimation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
 
 app.get('/users/:userId/courses/', (req, res) => {
